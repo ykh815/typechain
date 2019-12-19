92cd66ec2f6ed9e26cae767732e72f5ec29a23ef
Kwnghyun Yoo <ykh815@hotmail.com>
2019-12-19 21:48:07 +0900

    #6 Classes on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "bewan",
-  age: 41,
-  gender: "male"
-};
+const person = new Human("bewan", 41, "male");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
