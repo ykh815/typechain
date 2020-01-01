ec309600a7c0d897cecf28d9fbd2b1babd04ef16
Kwnghyun Yoo <ykh815@hotmail.com>
2020-01-01 22:09:19 +0900

    #12 Conclusions'

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ class Block {
   }
 }
 
-const genesisBlock: Block = new Block(0, "afouwef", "", "Hello", 123456);
+const genesisBlock: Block = new Block(0, "zerobase", "", "Hello", 123456);
 
 let blockchain: Block[] = [genesisBlock];
 
@@ -64,6 +64,7 @@ const createNewBlock = (data: string): Block => {
     data,
     newTimestamp
   );
+  addBlock(newBlock);
   return newBlock;
 };
 
@@ -94,5 +95,11 @@ const addBlock = (candidateBlock: Block): void => {
     blockchain.push(candidateBlock);
   }
 };
+
+createNewBlock("second block");
+createNewBlock("third block");
+createNewBlock("fourth block");
+
+console.log(blockchain);
 
 export {};
